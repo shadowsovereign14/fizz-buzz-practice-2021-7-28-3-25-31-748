8503c12fa6db072ead744f12d6c9fe7500ef860b
ryan.jay.retutar <ryan.jay.retutar@oocl.com>
2022-09-01 14:04:57 +0800

    test: created test case for displaying regular number besides the given multiples of special numbers

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,17 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    void should_return_1_when_count_off_given_1() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String expected = "1";
+
+        String actual = fizzBuzz.countOff(1);
+
+        assertEquals(expected, actual);
+    }
 }

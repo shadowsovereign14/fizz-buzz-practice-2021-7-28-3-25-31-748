9adb5f97b173a45f8e827db322e320f901d69da5
ryan.jay.retutar <ryan.jay.retutar@oocl.com>
2022-09-01 14:44:23 +0800

    refactor: declare constants for the string result

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,17 +1,22 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+
+    public static final String FIZZ = "Fizz";
+    public static final String BUZZ = "Buzz";
+    public static final String WHIZZ = "Whizz";
+
     public String countOff(int order) {
         String result = "";
 
         if(order % 3 == 0){
-            result = "Fizz";
+            result = FIZZ;
         }
         if(order % 5 == 0) {
-            result += "Buzz";
+            result += BUZZ;
         }
         if(order % 7 == 0) {
-            result += "Whizz";
+            result += WHIZZ;
         }
         if(result.isEmpty()) {
             result = String.valueOf(order);

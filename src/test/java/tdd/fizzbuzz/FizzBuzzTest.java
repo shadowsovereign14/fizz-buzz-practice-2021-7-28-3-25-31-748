085e67e36a8e25f3416df06ae9e9f49de95724a0
ryan.jay.retutar <ryan.jay.retutar@oocl.com>
2022-09-01 14:08:58 +0800

    test: created a test case to return Fizz when the number is multiple of 3

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -5,12 +5,21 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
+    FizzBuzz fizzBuzz = new FizzBuzz();
     @Test
     void should_return_1_when_count_off_given_1() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         String expected = "1";
 
         String actual = fizzBuzz.countOff(1);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void should_return_Fizz_when_countOff_given_3() {
+        String expected = "Fizz";
+
+        String actual = fizzBuzz.countOff(3);
 
         assertEquals(expected, actual);
     }
